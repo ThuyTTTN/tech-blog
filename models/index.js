8a4dc8605d15e92b3588b6c7e981302f3f609521
Thuy Nguyen <ttruong06@gmail.com>
2022-05-22 13:48:50 -0400

    create Post model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 //importing the User.js model and exporting an object with it as a property
 
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+module.exports = { User, Post };
