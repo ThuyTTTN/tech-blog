e7c7e41ada362e01c351daa9523cfd46a9c76c04
Thuy Nguyen <ttruong06@gmail.com>
2022-05-22 12:21:01 -0400

    routes/controllers and models folders

---
+++ models/index.js
@@ -0,0 +1,5 @@
+//importing the User.js model and exporting an object with it as a property
+
+const User = require('./User');
+
+module.exports = { User };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const controllers = require('./controllers');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +13,14 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+//turn on routes
+app.use(controllers);
 
-app.listen(PORT, () => console.log('Now listening'));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//turn on connection to db and server
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
