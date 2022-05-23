d1410ff0c5d200d56739757bb0881825849f5381
Thuy Nguyen <ttruong06@gmail.com>
2022-05-22 23:18:44 -0400

    comments

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,23 +1,32 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
-    order: [['created_at', 'DESC']],
+    order: [["created_at", "DESC"]],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+    ],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "username", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
       },
     ],
-  })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  });
 });
 
 router.get("/:id", (req, res) => {
@@ -95,7 +104,7 @@ router.delete("/:id", (req, res) => {
         res.status(404).json({ message: "No post found with this id" });
         return;
       }
-      res.json({message: "Post has been deleted!"});
+      res.json({ message: "Post has been deleted!" });
     })
     .catch((err) => {
       console.log(err);
--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
 // create associations
 User.hasMany(Post, {
@@ -12,4 +13,20 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
